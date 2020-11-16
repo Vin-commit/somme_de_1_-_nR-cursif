66c132f1206983b70030ae2336ab69261de848e6
Vin-commit <74498336+Vin-commit@users.noreply.github.com>
2020-11-16 20:09:21 +0100

    Add files via upload

---
+++ somme_de_1_à_nRécursif.py
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/python3
+# coding : utf-8
+
+
+def somme (n, maString):
+  if (n == 1):
+    print (maString+"1 soit ", end='')
+    return 1
+  if (n > 2):
+    maString += str(n-1)+"+"
+  return n + somme (n-1, maString) # Retourne toujours un réel et c’est impératif pour la récursivité.
+
+
+try:
+  n = int(input("Entrez le nombre dont on veut la somme avec tous ses précédents : "))
+  print (str(somme (n, "La somme des chiffres de 1 à "+str(n)+" est "+str(n)+"+"))+".")
+except:
+  print ("Veuillez saisir un nombre. ")
+
+
+----------------------------------------------------------------------- Résultat ------------------------------------------------------------------------------
